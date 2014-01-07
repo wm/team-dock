eba35288bcce5453837fbed0e7d142a073a95aef
Will Mernagh <wmernagh@gmail.com>
2014-01-07 16:38:48 +0100

    Add debugger

--- server.go
+++ server.go
@@ -3,21 +3,33 @@ package main
 import (
 	"github.com/codegangsta/martini"
 	"net/http"
+	"net/url"
 	"fmt"
 )
 
 type Build struct {
-	Branch        string   `json:"branch,omitempty"`
-	Project       string   `json:"project,omitempty"`
-	Result        string   `json:"result,omitempty"`
-	Status        string   `json:"status,omitempty"`
-	BuileNumber   string   `json:"buileNumber,omitempty"`
-	TriggeredBy   string   `json:"triggeredBy,omitempty"`
-	BuildTypeId   string   `json:"buildTypeId,omitempty"`
-	BuildId       string   `json:"buildId,omitempty"`
+	BuildStatusPrevious   string   `json:"buildStatusPrevious,omitempty"`
+	BuildId               string   `json:"buildId,omitempty"`
+	TriggeredBy           string   `json:"triggeredBy,omitempty"`
+	BuildStatus           string   `json:"buildStatus,omitempty"`
+	AgentHostname         string   `json:"agentHostname,omitempty"`
+	BuildFullName         string   `json:"buildFullName,omitempty"`
+	BuildTypeId           string   `json:"buildTypeId,omitempty"`
+	Message               string   `json:"message,omitempty"`
+	Text                  string   `json:"text,omitempty"`
+	NotifyType            string   `json:"notifyType,omitempty"`
+	AgentName             string   `json:"agentName,omitempty"`
+	BuildResult           string   `json:"buildResult,omitempty"`
+	BuildRunner           string   `json:"buildRunner,omitempty"`
+	ProjectId             string   `json:"projectId,omitempty"`
+	ProjectName           string   `json:"projectName,omitempty"`
+	AgentOs               string   `json:"agentOs,omitempty"`
+	BuildNumber           string   `json:"buildNumber,omitempty"`
+	BuildName             string   `json:"buildName,omitempty"`
 }
-	
+
 var lastBuild = Build{}
+var lastForm = url.Values{} //map[string]string{}
 
 func main() {
 	m := martini.New()
@@ -43,22 +55,32 @@ func Welcome() string {
 }
 
 func LastStatus() string {
-	return fmt.Sprintf("Build: %+v", lastBuild)
+	return fmt.Sprintf("Build: %+v, %+v", lastBuild, lastForm)
 }
 
 func Status(r *http.Request, w http.ResponseWriter) (result string) {
 	currentBuild := Build{
-		Branch:      r.FormValue("branch"),
-		Project:     r.FormValue("project"),
-		Result:      r.FormValue("result"),
-		Status:      r.FormValue("status"),
-		BuileNumber: r.FormValue("number"),
-		TriggeredBy: r.FormValue("by"),
-		BuildTypeId: r.FormValue("typeId"),
-		BuildId:     r.FormValue("buildId"),
+		BuildStatusPrevious: r.FormValue("buildStatusPrevious"),
+		BuildId:             r.FormValue("buildId"),
+		TriggeredBy:         r.FormValue("triggeredBy"),
+		BuildStatus:         r.FormValue("buildStatus"),
+		AgentHostname:       r.FormValue("agentHostname"),
+		BuildFullName:       r.FormValue("buildFullName"),
+		BuildTypeId:         r.FormValue("buildTypeId"),
+		Message:             r.FormValue("message"),
+		Text:                r.FormValue("text"),
+		NotifyType:          r.FormValue("notifyType"),
+		AgentName:           r.FormValue("agentName"),
+		BuildResult:         r.FormValue("buildResult"),
+		BuildRunner:         r.FormValue("buildRunner"),
+		ProjectId:           r.FormValue("projectId"),
+		ProjectName:         r.FormValue("projectName"),
+		AgentOs:             r.FormValue("agentOs"),
+		BuildNumber:         r.FormValue("buildNumber"),
+		BuildName:           r.FormValue("buildName"),
 	}
 
-	result = fmt.Sprintf("Build: %+v", currentBuild)
 	lastBuild = currentBuild
-	return result
+	lastForm = r.Form
+	return "ok"
 }
